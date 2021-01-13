85a273513903caf74d9a2f971b34e00cbfa5b1d8
MaVeRohoho <76155685+MaVeRohoho@users.noreply.github.com>
2021-01-13 21:32:13 +0100

    updated java fixed location button
    included a background image

--- script.js
+++ script.js
@@ -3,7 +3,21 @@ function search (city){
 let apiKey = "fc50e00c9bbae52d3e97a4dfd4c8a5f5";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemp);
+
+let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+xios.get(apiUrl).then(displayForecast);
 }
+
+function displayForecast (response) {
+    let forecastElement = document.querySelector ("#forecast");
+    
+    forecastElement.innerHTML = ``
+
+}
+
+
+
+
 function formatDate (timestamp){
     let date = new Date (timestamp);
     let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday" ];
@@ -18,12 +32,22 @@ function formatDate (timestamp){
     }
     return `${day}, ${hours}:${minutes}`;
 }
+function searchCoords (position){
+let apiKey = "fc50e00c9bbae52d3e97a4dfd4c8a5f5";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}`;
+axios.get(apiUrl).then(displayWeather);
+}
+function getCurrentLocation (event) {
+event. preventDefault ();
+navigator.geolocation.getCurrentPosition (searchCoords);
+}
+let myWeatherButton = document.querySelector  ("#my-weather-button");
+myWeatherButton .addEventListener ("click", getCurrentLocation);
+
 function displayTemp (response) {
     let temperatureElement = document.querySelector ("#temperature");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);    
-
     celciusTemp = response.data.main.temp;
-
     let cityElement = document.querySelector ("#city");
     cityElement.innerHTML = response.data.name;    
     let descriptionElement = document.querySelector ("#description");
@@ -34,20 +58,21 @@ function displayTemp (response) {
     humidElement.innerHTML = response.data.main.humidity; 
     let dateElement = document.querySelector ("#date");
     dateElement.innerHTML = formatDate (response.data.dt * 1000);     
-    let iconElement = document.querySelector ("#iconOpenWeather")     
-    iconElement.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` ) 
-    let earlyIcon = document.querySelector ("#iconOpenWeatherEarly")     
-    earlyIcon.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` ) 
+    let iconElement = document.querySelector ("#iconOpenWeather") ;    
+    iconElement.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` ) ;
+
+    let earlyIcon = document.querySelector ("#iconOpenWeatherEarly") ;    
+    earlyIcon.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` ) ;
     let morningIcon = document.querySelector ("#iconOpenWeatherMorning")     
-    morningIcon.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` ) 
+    morningIcon.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` ); 
     let lunchIcon = document.querySelector ("#iconOpenWeatherLunch")     
-    lunchIcon.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` ) 
+    lunchIcon.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` ); 
     let afternoonIcon = document.querySelector ("#iconOpenWeatherAfternoon")     
-    afternoonIcon.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` ) 
+    afternoonIcon.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` ) ;
     let eveningIcon = document.querySelector ("#iconOpenWeatherEvening")     
-    eveningIcon.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` ) 
+    eveningIcon.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` ) ;
     let nightIcon = document.querySelector ("#iconOpenWeatherNight")     
-    nightIcon.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` ) 
+    nightIcon.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` ) ;
 
     let mainIcon = document.querySelector("#mainIcon");
     if(response.data.weather[0].icon === "01d") {
@@ -93,20 +118,6 @@ function handleSubmit (event){
      let cityInputElement = document.querySelector("#city-input");
      search(cityInputElement.value);
 }
-function searchCoords (position){
-let apiKey = "fc50e00c9bbae52d3e97a4dfd4c8a5f5";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}`;
-axios.get(apiUrl).then(displayTemp);
-}
-
-function getCurrentLocation (event) {
-    event. preventDefault ();
-    navigator.geolocation.getCurrentPosition(searchCoords);
-}
-
-let findMeButton = document.querySelector ("#my-weather-button");
-findMeButton .addEventListener ("click", getCurrentLocation);
-
 
 function displayFahrenheit (event) {
     event.preventDefault();
@@ -129,6 +140,5 @@ let celciusTemp= null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
-
 
 search ("Zurich"); 
